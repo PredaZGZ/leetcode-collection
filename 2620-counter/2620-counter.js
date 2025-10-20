3fb78c621cf8023a4fc30780ec67b531c290271c
Saúl Moreno <51711046+PredaZGZ@users.noreply.github.com>
2025-10-20 22:28:00 +0200

    Time: 23 ms (99.11%), Space: 54 MB (39.93%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -2,7 +2,8 @@
  * @param {number} n
  * @return {Function} counter
  */
+ 
 function createCounter(n) {
-  let v = n | 0;            // fuerza Small Integer (SMI)
-  return function() { return v++; };
+  let v = n | 0; // fuerza Small Integer (SMI)
+  return function() { return v++; }; 
 }
