f349384c3d28e615dd971da03a739cbcc4756eb9
Saúl Moreno <51711046+PredaZGZ@users.noreply.github.com>
2025-12-23 23:52:40 +0100

    Time: 1 ms (99.88%), Space: 43.6 MB (64.08%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,34 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
-        final int n = s.length();
-        int i = 0;
-
+        int n = s.length(), i = 0;
         while (i < n && s.charAt(i) == ' ') i++;
 
         int sign = 1;
         if (i < n) {
             char c = s.charAt(i);
             if (c == '-') { sign = -1; i++; }
-            else if (c == '+') { i++; }
+            else if (c == '+') i++;
         }
 
-        final int limit = (sign == 1) ? -2147483647 : -2147483648;
-        final int multMin = -214748364;              
-        final int lastDigit = (sign == 1) ? 7 : 8;  
+        int limit = (sign == 1) ? -Integer.MAX_VALUE : Integer.MIN_VALUE;
+        int multMin = limit / 10;
+        int limLast = -(limit % 10);
 
-        int res = 0; // negativo
+        int res = 0;
         while (i < n) {
-            char c = s.charAt(i);
-            int d = c - '0';
-            if (d < 0 || d > 9) break;
+            int d = s.charAt(i) - '0';
+            if ((d | (9 - d)) < 0) break;
 
-            if (res < multMin || (res == multMin && d > lastDigit))
-                return (sign == 1) ? 2147483647 : -2147483648;
+            if (res < multMin || (res == multMin && d > limLast))
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
 
             res = res * 10 - d;
             i++;
         }
 
-        return (sign == 1) ? -res : res;
+        return sign == 1 ? -res : res;
     }
 }
